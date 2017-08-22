5ef51874983a4eedb54adf304285f0f21c2f0d8e
Jacob Lambert <jacob.lambert@clearlink.com>
2017-08-22 15:13:51 -0600

    Add Benchmark to test function with arguements

--- part_test.go
+++ part_test.go
@@ -21,3 +21,22 @@ func BenchmarkPartAppendToSQL(b *testing.B) {
 		appendToSql(parts, sql, ", ", make([]interface{}, 0))
 	}
 }
+
+func BenchmarkPartWithArguementAppendToSQL(b *testing.B) {
+	parts := []Sqlizer{
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1),
+		newPart("test", 1)}
+
+	for n := 0; n < b.N; n++ {
+		sql := &bytes.Buffer{}
+		appendToSql(parts, sql, ", ", make([]interface{}, 0))
+	}
+}
